98bb4d2b6a3ac782eca584d026420e9c30faafcd
Chikeluba Obiora-Eze <chikelubaobioraeze@gmail.com>
2025-07-12 19:15:49 +0100

    `Add koa-compose as a dev dependency

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['src/index.ts'],
     format: ['cjs', 'esm'],
+    external: ['koa', 'koa-compose'],
     dts: true,
     sourcemap: true,
     clean: true,
